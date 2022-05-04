e8fc86d1d54e743d67001831d073568d62d33094
Alan Cardoso de Almeida <37117478+Card68@users.noreply.github.com>
2022-05-04 12:01:17 -0300

    Update Address.java
    utilização do lombok Getter Setter e toString para reduzir as linhas de códigos desses métodos

--- src/main/java/org/agoncal/application/petstore/domain/Address.java
+++ src/main/java/org/agoncal/application/petstore/domain/Address.java
@@ -4,6 +4,9 @@ import javax.persistence.Column;
 import javax.persistence.Embeddable;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import lombok Getter
+import lombok Setter
+import lombok toString
 
 /**
  * @author Antonio Goncalves
@@ -52,58 +55,6 @@ public class Address {
     }
 
     // ======================================
-    // =         Getters & setters          =
-    // ======================================
-
-    public String getStreet1() {
-        return street1;
-    }
-
-    public void setStreet1(String street1) {
-        this.street1 = street1;
-    }
-
-    public String getStreet2() {
-        return street2;
-    }
-
-    public void setStreet2(String street2) {
-        this.street2 = street2;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getState() {
-        return state;
-    }
-
-    public void setState(String state) {
-        this.state = state;
-    }
-
-    public String getZipcode() {
-        return zipcode;
-    }
-
-    public void setZipcode(String zipcode) {
-        this.zipcode = zipcode;
-    }
-
-    public String getCountry() {
-        return country;
-    }
-
-    public void setCountry(String country) {
-        this.country = country;
-    }
-
-    // ======================================
     // =   Methods hash, equals, toString   =
     // ======================================
 
@@ -135,17 +86,4 @@ public class Address {
         return result;
     }
 
-    @Override
-    public String toString() {
-        final StringBuilder sb = new StringBuilder();
-        sb.append("Address");
-        sb.append("{street1='").append(street1).append('\'');
-        sb.append(", street2='").append(street2).append('\'');
-        sb.append(", city='").append(city).append('\'');
-        sb.append(", state='").append(state).append('\'');
-        sb.append(", zipcode='").append(zipcode).append('\'');
-        sb.append(", country='").append(country).append('\'');
-        sb.append('}');
-        return sb.toString();
-    }
 }
